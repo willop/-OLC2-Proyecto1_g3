dc2fac96faae8ebabba2f1240aa1a9e03598d9eb
WillOP <wilfredp159@gmail.com>
2021-12-07 12:53:33 -0600

    Actualizacion de pagina usando CodeMirror

--- functions.js
+++ functions.js
@@ -3,13 +3,24 @@ import {Imprimir} from "./functions2.js"
 //boton de importar 
 document.querySelector('#File1').addEventListener('change', leerarchivo);
 
-document.getElementById('TextoEdicion').addEventListener('change',updateContent,false);
 
 //variables globales
 var TextoDeEdicion = "";
-//const parser = require('./gramatica.js');
 
+var editor = CodeMirror.fromTextArea
+(document.getElementById('TextoEdicion'),{
+    mode: "javascript",
+    theme: "3024-night",
+    lineNumbers: true,
+});
+editor.setSize(480,757);
 
+editor.on('change',function(asd) {
+    TextoDeEdicion = asd.getValue();
+    console.log(TextoDeEdicion);
+})
+
+            
 //funcion para leer archivos
 function leerarchivo(e) {
     const archivo = e.target.files[0];
@@ -25,6 +36,8 @@ function leerarchivo(e) {
     lector.readAsText(archivo);
 }
 
+
+editor.setSize(480,757);
 //funcion para mostrar el contenido en el textbox
 function mostrarcontenido(_contenido) {
     const elemento = document.getElementById('TextoEdicion');
@@ -34,8 +47,8 @@ function mostrarcontenido(_contenido) {
 
 //funcion para actualizar el contenido de la edicion 
 function updateContent(e){
-    TextoDeEdicion = document.getElementById('TextoEdicion').value;
-    //console.log(TextoDeEdicion);
+    TextoDeEdicion = editor.getvalue();
+    alert(TextoDeEdicion);
 }
 
 
@@ -43,9 +56,10 @@ document.getElementById('boton_Compilar').addEventListener('click', accionBoton)
 
 
 function accionBoton(e){
-    Imprimir("Hola mundo desde functions");
     const otrotext = document.getElementById('textarea2');
+    //TextoDeEdicion = editor.getValue();
     otrotext.innerHTML = TextoDeEdicion;
+    Imprimir(TextoDeEdicion);
     console.log(TextoDeEdicion);
     gramatica.parse(TextoDeEdicion);
 }
